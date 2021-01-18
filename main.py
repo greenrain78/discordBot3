099a4c3a23658c7706c757bf028a69b57ee753ef
daewon kim <kimdw010130@daum.net>
2021-01-18 14:12:58 +0900

    github push test

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from Log.infoLog import logger as log
 token = discord_token
 
 if __name__ == '__main__':
+    log.info('start programe')
     client = MyBot()
 
     @client.event
     async def on_ready():
         log.info('Logged in as {0} ({0.id})'.format(client.user))
-        log.debug('test11')
 
     client.run(discord_token)
