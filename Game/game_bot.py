05cd8101578335e125494608d1adb07830b1407b
daewon kim <kimdw010130@daum.net>
2021-02-03 05:30:02 +0900

    game bot add function update user point by game result

--- Game/game_bot.py
+++ Game/game_bot.py
@@ -1,25 +1,37 @@
 import asyncio
 from discord.ext import commands
 from Log.infoLog import logger as log
-from Game.game_engine import GameEngine
+from Game import game_engine as engine
+from Point import user_DB
 
 
 class GameBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.engine = GameEngine()
 
     @commands.command()
-    async def game(self, ctx, name, *args):
+    async def game(self, ctx, point, game_name, *args):
         """
         게임
         1. 홀짝 게임
         """
         try:
-            if name == "홀짝":
-                text = self.engine.odd_even(args)
-            else:
-                text = "해당 게임이 존재하지 않습니다."
+            user = ctx.author.name
+            try:
+                pt = int(point)
+                mypoint = user_DB.get_point(user)
+                if mypoint <= pt:
+                    text = f"{user}님 보유 포인트가 부족합니다.\n" \
+                           f"입력하신 포인트: {pt}, 보유 포인트: {mypoint}"
+
+                elif game_name == "홀짝":
+                    text = engine.odd_even(user, pt, args)
+
+                else:
+                    text = "해당 게임이 존재하지 않습니다."
+
+            except ValueError:
+                text = f'{point}는 숫자가 아닙니다.'
 
             msg = await ctx.send(text)
             await asyncio.sleep(60)
@@ -28,5 +40,3 @@ class GameBot(commands.Cog):
             await msg.delete()  # 메세지 삭제
         except Exception as e:
             log.exception("command botState error")
-
-
--- Game/game_engine.py
+++ Game/game_engine.py
@@ -1,5 +1,7 @@
 import random
 
+from Point import user_DB, point_DB
+
 
 def check_wrong_args(args, num):
     if len(args) != num:
@@ -8,35 +10,53 @@ def check_wrong_args(args, num):
     return False
 
 
-class GameEngine:
-    def __init__(self):
-        pass
+def update_point(game_name, user, point):
+    pt = user_DB.get_point(user)
+    result = pt + point
+    reason = f"게임({game_name})으로 포인트:{point}만큼 획득하셨습니다."
 
-    def odd_even(self, args):
-        # 인자 확인
-        check = check_wrong_args(args, 1)
-        if check:
-            return check
-        num = args[0]
-        if num != "홀" and num != "짝":
-            text = f"홀짝 게임에서는 홀 짝 둘중 하나만 선택할 수 있어요.\n 알겠나요?"
-            return text
+    user_DB.update_user_point(user, result)
+    point_DB.insert(user, point, reason, result)
 
-        # 홀짝
-        rand = random.randrange(1, 3)
-        # 홀
-        if rand == 1:
-            if num == "홀":
-                text = f"정답은 홀!!! \n정답입니다!!"
-            elif num == "짝":
-                text = f"정답은 홀!!! \n틀렸습니다ㅠㅠ"
-        # 짝
-        elif rand == 2:
-            if num == "홀":
-                text = f"정답은 짝!!! \n틀렸습니다ㅠㅠ"
-            elif num == "짝":
-                text = f"정답은 짝!!! \n정답입니다!!"
+    text = f"획득 포인트: {point}, 총 포인트: {result}"
+    return text
+
+
+def odd_even(user, point, args):
+    # 인자 확인
+    check = check_wrong_args(args, 1)
+    if check:
+        return check
+    num = args[0]
+    if num != "홀" and num != "짝":
+        text = f"홀짝 게임에서는 홀 짝 둘중 하나만 선택할 수 있어요.\n 알겠나요?"
         return text
 
-    def gameCount_user(self, user):
-        pass
+    # 홀짝
+    rand = random.randrange(1, 3)
+    # 홀
+    if rand == 1:
+        if num == "홀":
+            text = f"정답은 홀!!! \n정답입니다!!\n"
+            pt = point
+        elif num == "짝":
+            text = f"정답은 홀!!! \n틀렸습니다ㅠㅠ\n"
+            pt = -point
+
+    # 짝
+    elif rand == 2:
+        if num == "홀":
+            text = f"정답은 짝!!! \n틀렸습니다ㅠㅠ\n"
+            pt = -point
+        elif num == "짝":
+            text = f"정답은 짝!!! \n정답입니다!!\n"
+            pt = point
+
+    # 점수 처리
+    text += update_point("홀짝 게임", user, pt)
+
+    return text
+
+
+def gameCount_user(user):
+    pass
